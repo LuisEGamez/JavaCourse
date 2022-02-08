ced35a4463ed6829e7a5898ba2040d589ed2a7dc
LuisEGamez <luisgamezdvp@gmail.com>
2022-02-08 09:46:49 +0100

    Test no generic methods

---
+++ Sprint1/Tasca6/src/n1Excercise1/App.java
@@ -0,0 +1,10 @@
+package n1Excercise1;
+
+public class App {
+    public static void main(String[] args) {
+
+        NoGenericMethods noGenericMethods = new NoGenericMethods("Hola", "Adios", "Papa");
+
+        NoGenericMethods noGenericMethods1 = new NoGenericMethods("Adios", "Papa", "Hola");
+    }
+}
