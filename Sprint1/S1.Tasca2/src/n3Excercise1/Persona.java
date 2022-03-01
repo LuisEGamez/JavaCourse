3e4d8beb2b60bb1568ec053a4cd9f0829c01e1f0
LuisEGamez <92061092+LuisEGamez@users.noreply.github.com>
2022-03-01 11:54:54 +0100

    Delete Sprint1/S1.Tasca2/src directory

--- Sprint1/S1.Tasca2/src/n3Excercise1/Persona.java
+++
@@ -1,8 +0,0 @@
-package n3Excercise1;
-
-public class Persona {
-
-    public Persona () throws ArithmeticException{
-        throw new ArithmeticException("Exception lanzada por la clase padre");
-    }
-}
